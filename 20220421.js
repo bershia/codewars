439c110ab0493f28de4d4034b3bb284d00bff169
bershia <iamnica@gmail.com>
2022-04-21 18:15:09 -0400

    3 challanges completed

---
+++ 20220421.js
@@ -0,0 +1,28 @@
+// 1. The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+// Input
+// Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+// Output
+// Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+function openOrSenior(data){
+    return data.map(pair => pair[0] > 54 && pair[1] > 7 ? 'Senior' : 'Open')
+}
+
+
+// 2. Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+const binaryArrayToNumber = arr => parseInt(arr.join(''), 2)
+
+
+// 3. There is a bus moving in the city, and it takes and drop some people in each bus stop.
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+// Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+// Take a look on the test cases.
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+// The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+let number = function(busStops){
+    return busStops.reduce((sum, curVal) => sum + curVal[0] - curVal[1], 0)
+}
