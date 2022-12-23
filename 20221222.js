c4dc834ba51b248abc63a9ecda43ee85b002630e
nika hughes <iamnica@gmail.com>
2022-12-22 22:28:04 -0500

    1 challange completed

---
+++ 20221222.js
@@ -0,0 +1,13 @@
+// CamelCase Method
+
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
+// Don't forget to rate this kata! Thanks :)
+
+String.prototype.camelCase = function(){
+    return this.split(' ').map(x => x.charAt(0).toUpperCase() + x.slice(1)).join('')
+}
